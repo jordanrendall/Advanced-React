4480c3e3976839e8ff908057fcb568eb9e08b6e5
jordanrendall <thomasjrendall@gmail.com>
2019-03-15 06:36:52 -0400

    Created a page theme using styled components

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,15 +1,42 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Meta from "./Meta";
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
+
+const theme = {
+  //needs quotes since these are strings, not normal css
+  red: "#FF0000",
+  silver: "silver",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightgrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)"
+};
+
+const StyledPage = styled.div`
+  background: ${props => props.theme.silver};
+  color: black;
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  background: red;
+  padding: 2rem;
+`;
 
 export class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
