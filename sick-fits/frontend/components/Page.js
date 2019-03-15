ed1ad0659328c692be767a77ebef0b95102b7bbc
jordanrendall <thomasjrendall@gmail.com>
2019-03-15 05:41:20 -0400

    Created top level Page component

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Page extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hey I'm the page component!</p>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+export default Page
