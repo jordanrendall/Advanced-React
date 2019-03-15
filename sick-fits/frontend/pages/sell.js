49a6f3f8f8ea724137c809ed3d4661cea019fe80
jordanrendall <thomasjrendall@gmail.com>
2019-03-14 20:27:46 -0400

    Added links between home and sell

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,7 +1,11 @@
+import Link from 'next/link';
 
 const Sell = props => (
     <div>
         <p>Sell!</p>
+        <Link href="/sell">
+            <a>Sell</a>
+        </Link>
     </div>
 );
    
