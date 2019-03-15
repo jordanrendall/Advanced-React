293edc5c8a2cdaa799e93e1b00c980e2d340ad29
jordanrendall <thomasjrendall@gmail.com>
2019-03-15 06:09:50 -0400

    Introduced styled components to page

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import Meta from "./Meta";
+import styled from "styled-components";
+
+const MyButton = styled.button`
+  background: red;
+  font-size: ${props => (props.huge ? "100px" : "50px")};
+  span {
+    font-size: 100px;
+  }
+`;
+
+const BigPoop = styled.span`
+  font-size: 100px;
+`;
 
 export class Page extends Component {
   render() {
     return (
       <div>
+        <Meta />
         <Header />
+        <MyButton huge>
+          Click me
+          <span>💩</span>
+        </MyButton>
+        <MyButton>Second smaller button</MyButton>
         {this.props.children}
       </div>
     );
