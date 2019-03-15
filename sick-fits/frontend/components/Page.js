c60b43dacc6a3bcfb0d3940bf1ef8b9d4ad653bb
jordanrendall <thomasjrendall@gmail.com>
2019-03-15 06:10:42 -0400

    Removed test styled components from page

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,19 +1,6 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Meta from "./Meta";
-import styled from "styled-components";
-
-const MyButton = styled.button`
-  background: red;
-  font-size: ${props => (props.huge ? "100px" : "50px")};
-  span {
-    font-size: 100px;
-  }
-`;
-
-const BigPoop = styled.span`
-  font-size: 100px;
-`;
 
 export class Page extends Component {
   render() {
@@ -21,11 +8,6 @@ export class Page extends Component {
       <div>
         <Meta />
         <Header />
-        <MyButton huge>
-          Click me
-          <span>💩</span>
-        </MyButton>
-        <MyButton>Second smaller button</MyButton>
         {this.props.children}
       </div>
     );
