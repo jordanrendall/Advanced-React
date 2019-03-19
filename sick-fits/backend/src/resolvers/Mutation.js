80f89b1ba8d8478eb485a3f80392222607b961c8
jordanrendall <thomasjrendall@gmail.com>
2019-03-18 20:10:25 -0400

    Updated mutation resolver to include createItem mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,24 @@
-const Mutations = {};
+const Mutations = {
+  //   createDog(parent, args, ctx, info) {
+  //     global.dogs = global.dogs || [];
+  //     console.log(args);
+  //     const newDog = { name: args.name };
+  //     global.dogs.push(newDog);
+  //     return newDog;
+  //   }
+  async createItem(parent, args, ctx, info) {
+    //TODO: check if logged in
+    ctx.db.mutation.createItem;
+    item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
+};
 
 module.exports = Mutations;
