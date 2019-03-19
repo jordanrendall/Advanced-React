6044187b9ad4d3be965b6f87c30e9d1537a25198
jordanrendall <thomasjrendall@gmail.com>
2019-03-18 20:09:12 -0400

    Created server

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,22 @@
-// let's go!
+//Expose environment variables file to this index file
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+//spin up server
+const server = createServer();
+
+//TODO: use express middleware to handle cookies (JWT)
+//TODO: use express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is now running on port http:localhost:${deets.port}`);
+  }
+);
