5a18ff3c9b107d951b2c15ac87763e6bf1d03b14
jordanrendall <thomasjrendall@gmail.com>
2019-03-20 20:31:58 -0400

    Added forwarding to db for single item query

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -2,7 +2,8 @@ const { forwardTo } = require("prisma-binding");
 
 const Query = {
   //for before using authentication or any extra work on the yoga side, can forward directly to prisma db using forwardTo
-  items: forwardTo("db")
+  items: forwardTo("db"),
+  item: forwardTo("db")
   //   async items(parent, args, ctx, info) {
   //     const items = await ctx.db.query.items();
   //     return items;
