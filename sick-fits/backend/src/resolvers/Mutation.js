bc3423402d04d38b434bf4ec4637bae10d27e98c
jordanrendall <thomasjrendall@gmail.com>
2019-03-22 07:40:43 -0400

    Added deleteItemMutation to resolvers

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -38,6 +38,15 @@ const Mutations = {
       },
       info
     );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    //1. find item
+    const item = await ctx.db.query.item({ where }, `{id title}`);
+    //2. check if they own or have permissions
+    // TODO
+    //3. delete it
+    return await ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
