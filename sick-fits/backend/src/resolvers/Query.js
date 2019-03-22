c1f73097fc859f835a0a4007f7046dc2eff30ee3
jordanrendall <thomasjrendall@gmail.com>
2019-03-22 07:41:04 -0400

    Added query resolver forward for itemsConnection

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -3,7 +3,8 @@ const { forwardTo } = require("prisma-binding");
 const Query = {
   //for before using authentication or any extra work on the yoga side, can forward directly to prisma db using forwardTo
   items: forwardTo("db"),
-  item: forwardTo("db")
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db")
   //   async items(parent, args, ctx, info) {
   //     const items = await ctx.db.query.items();
   //     return items;
