e365300fbe34a2d5b023dbbb19927778e84f31d6
jordanrendall <thomasjrendall@gmail.com>
2019-03-25 06:29:39 -0400

    Added JWT decode middleware

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,5 +1,6 @@
 //THIS FILE IS JUST EXPRESS
 const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 //Expose environment variables file to this index file
 require("dotenv").config({ path: "variables.env" });
 const createServer = require("./createServer");
@@ -11,7 +12,16 @@ const server = createServer();
 //Use express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
 
-//TODO: use express middleware to populate current user
+//Decode the JWT so we can get userID on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    //put userId on to req for further requests to access
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
