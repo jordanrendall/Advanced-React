9a5673d20ac30eed6c3eae32def6ac16e62fa419
jordanrendall <thomasjrendall@gmail.com>
2019-04-08 18:34:03 -0400

    Added middlewhere to look for user permissions before use of the site

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -23,6 +23,19 @@ server.express.use((req, res, next) => {
   next();
 });
 
+//Create a middleware that populates the user on each request
+server.express.use(async (req, res, next) => {
+  //if not logged in, return next();
+  if (!req.userId) return next();
+  const user = await db.query.user(
+    { where: { id: req.userId } },
+    "{ id, permissions, email, name }"
+  );
+  console.log(user);
+  req.user = user;
+  next();
+});
+
 server.start(
   {
     cors: {
