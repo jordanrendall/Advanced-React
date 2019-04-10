ac6320dd0b8aa4d02c304013ed6ead9c787d1816
jordanrendall <thomasjrendall@gmail.com>
2019-04-10 06:24:22 -0400

    Created local state query to check if the cart is open

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -1,22 +1,34 @@
 import React from "react";
+import { Query, Mutation } from "react-apollo";
+import gql from "graphql-tag";
 import CartStyles from "./styles/CartStyles";
 import Supreme from "./styles/Supreme";
 import CloseButton from "./styles/CloseButton";
 import SickButton from "./styles/SickButton";
 
+const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`;
+
 const Cart = () => {
   return (
-    <CartStyles open>
-      <header>
-        <CloseButton title="close">&times;</CloseButton>
-        <Supreme>Your Cart</Supreme>
-        <p>You Have __ Items in your Cart</p>
-      </header>
-      <footer>
-        <p>$10.10</p>
-        <SickButton>Checkout</SickButton>
-      </footer>
-    </CartStyles>
+    <Query query={LOCAL_STATE_QUERY}>
+      {({ data }) => (
+        <CartStyles open={data.cartOpen}>
+          <header>
+            <CloseButton title="close">&times;</CloseButton>
+            <Supreme>Your Cart</Supreme>
+            <p>You Have __ Items in your Cart</p>
+          </header>
+          <footer>
+            <p>$10.10</p>
+            <SickButton>Checkout</SickButton>
+          </footer>
+        </CartStyles>
+      )}
+    </Query>
   );
 };
 
