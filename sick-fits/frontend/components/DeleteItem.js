1eebc6fd75eb21758977bb35fb099aecd843e6b6
jordanrendall <thomasjrendall@gmail.com>
2019-04-10 06:17:51 -0400

    Added error handling for deleteItem component

--- sick-fits/frontend/components/DeleteItem.js
+++ sick-fits/frontend/components/DeleteItem.js
@@ -35,7 +35,9 @@ export default class DeleteItem extends Component {
           <button
             onClick={() => {
               if (confirm("Are you sure you want to delete this?")) {
-                deleteItem();
+                deleteItem().catch(err => {
+                  alert(err.message);
+                });
               }
             }}
           >
