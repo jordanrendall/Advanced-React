53b8d7231c6eb20a7b9f4778e1bf1d1225caeee7
jordanrendall <thomasjrendall@gmail.com>
2019-04-10 06:37:39 -0400

    Added toggle cart funcitonality using local state mutation

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Mutation } from "react-apollo";
+import { TOGGLE_CART_MUTATION } from "./Cart";
 import Link from "next/link";
 import NavStyles from "./styles/NavStyles";
 import User from "./User";
@@ -26,6 +28,9 @@ export default function Nav() {
                 <a>Me</a>
               </Link>
               <Signout />
+              <Mutation mutation={TOGGLE_CART_MUTATION}>
+                {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+              </Mutation>
             </>
           )}
 
