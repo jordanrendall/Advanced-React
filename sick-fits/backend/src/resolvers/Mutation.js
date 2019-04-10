d439e0c6ac473eeb4e7726d24a4d6ff66b385c31
jordanrendall <thomasjrendall@gmail.com>
2019-04-10 06:17:13 -0400

    Added permissions check in deleteItem mutation resolver

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -58,9 +58,16 @@ const Mutations = {
   async deleteItem(parent, args, ctx, info) {
     const where = { id: args.id };
     //1. find item
-    const item = await ctx.db.query.item({ where }, `{id title}`);
+    const item = await ctx.db.query.item({ where }, `{id title user {id}}`);
     //2. check if they own or have permissions
-    // TODO
+    const ownsItem = item.user.id === ctx.request.userId;
+    const hasPermissions = ctx.request.user.permissions.some(permission =>
+      ["ADMIN", "ITEMDELETE"].includes(permission)
+    );
+
+    if (!ownsItem && hasPermissions) {
+      throw new Error("You don't have permission to do that!");
+    }
     //3. delete it
     return await ctx.db.mutation.deleteItem({ where }, info);
   },
