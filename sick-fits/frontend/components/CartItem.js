f2874bddff7532e542794079d93a3f66ba5248a7
jordanrendall <thomasjrendall@gmail.com>
2019-04-11 06:50:17 -0400

    Created cartitem component

---
+++ sick-fits/frontend/components/CartItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import formatMoney from "../lib/formatMoney";
+
+const CartItemStyles = styled.li``;
+
+const CartItem = props => <CartItemStyles />;
+
+CartItem.propTypes = {
+  cartItem: PropTypes.object.isRequired
+};
+
+export default CartItem;
