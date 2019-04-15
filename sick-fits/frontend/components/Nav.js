88a5c31b61f9071f4c0e5f7eafe609f1eedcca78
jordanrendall <thomasjrendall@gmail.com>
2019-04-15 06:34:19 -0400

    Added cartcount to nav

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { Mutation } from "react-apollo";
-import { TOGGLE_CART_MUTATION } from "./Cart";
-import Link from "next/link";
-import NavStyles from "./styles/NavStyles";
-import User from "./User";
-import Signout from "./Signout";
+import React from 'react';
+import { Mutation } from 'react-apollo';
+import { TOGGLE_CART_MUTATION } from './Cart';
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
+import User from './User';
+import Signout from './Signout';
+import CartCount from './CartCount';
 
 export default function Nav() {
   return (
@@ -12,30 +13,39 @@ export default function Nav() {
     <User>
       {({ data: { me } }) => (
         <NavStyles>
-          <Link href="/items">
+          <Link href='/items'>
             <a>Shop</a>
           </Link>
           {me && (
             //react fragment invisible
             <>
-              <Link href="/sell">
+              <Link href='/sell'>
                 <a>Sell</a>
               </Link>
-              <Link href="/orders">
+              <Link href='/orders'>
                 <a>Orders</a>
               </Link>
-              <Link href="/me">
+              <Link href='/me'>
                 <a>Me</a>
               </Link>
               <Signout />
               <Mutation mutation={TOGGLE_CART_MUTATION}>
-                {toggleCart => <button onClick={toggleCart}>My Cart</button>}
+                {toggleCart => (
+                  <button onClick={toggleCart}>
+                    My Cart
+                    <CartCount
+                      count={me.cart.reduce((count, currentItem) => {
+                        return count + currentItem.quantity;
+                      }, 0)}
+                    />
+                  </button>
+                )}
               </Mutation>
             </>
           )}
 
           {!me && (
-            <Link href="/signup">
+            <Link href='/signup'>
               <a>Sign In</a>
             </Link>
           )}
