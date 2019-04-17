940c6dfef802bb3eb6d96f7e9df0fc41a80abd40
jordanrendall <thomasjrendall@gmail.com>
2019-04-17 05:54:33 -0400

    Used process variables instead of hard coded text for image sources

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -1,10 +1,12 @@
-import React, { Component } from "react";
-import { Mutation } from "react-apollo";
-import gql from "graphql-tag";
-import Router from "next/router";
-import Form from "./styles/Form";
-import formatMoney from "../lib/formatMoney";
-import Error from "./ErrorMessage";
+const dotenv = require('dotenv').config();
+
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Router from 'next/router';
+import Form from './styles/Form';
+import formatMoney from '../lib/formatMoney';
+import Error from './ErrorMessage';
 
 //CREATE part of CRUD
 const CREATE_ITEM_MUTATION = gql`
@@ -30,40 +32,37 @@ const CREATE_ITEM_MUTATION = gql`
 export default class CreateItem extends Component {
   //do not need constructor and this.state in ES6. Can just declare state ={}
   state = {
-    title: "",
-    description: "",
-    image: "",
-    largeImage: "",
-    price: 0 //can have trailing commas in ES6
+    title: '',
+    description: '',
+    image: '',
+    largeImage: '',
+    price: 0, //can have trailing commas in ES6
   };
 
   //can use arrow functions to get 'this' keyword assumed to be this object, and do not have to bind function to 'this'
   handleChange = e => {
     const { name, type, value } = e.target;
-    const val = type === "number" ? parseFloat(value) : value;
+    const val = type === 'number' ? parseFloat(value) : value;
     this.setState({
-      [name]: val
+      [name]: val,
     });
   };
 
   uploadFile = async e => {
-    console.log("Uploading...");
+    console.log('Uploading...');
     const files = e.target.files;
     const data = new FormData();
-    data.append("file", files[0]);
-    data.append("upload_preset", "sickfits");
-    const res = await fetch(
-      "https://api.cloudinary.com/v1_1/jordanrendall-cloudinary/image/upload",
-      {
-        method: "POST",
-        body: data
-      }
-    );
+    data.append('file', files[0]);
+    data.append('upload_preset', process.env.PRESET_NAME);
+    const res = await fetch(process.env.IMG_SOURCE, {
+      method: 'POST',
+      body: data,
+    });
     const file = await res.json();
     console.log(file);
     this.setState({
       image: file.secure_url,
-      largeImage: file.eager[0].secure_url
+      largeImage: file.eager[0].secure_url,
     });
   };
 
@@ -81,8 +80,8 @@ export default class CreateItem extends Component {
               //route user to the single item page
               console.log(res);
               Router.push({
-                pathname: "/item",
-                query: { id: res.data.createItem.id }
+                pathname: '/item',
+                query: { id: res.data.createItem.id },
               });
             }}
           >
@@ -91,59 +90,59 @@ export default class CreateItem extends Component {
               disabled={loading}
               aria-busy={loading} /*aria-busy is for accessibility*/
             >
-              <label htmlFor="file">
+              <label htmlFor='file'>
                 Image
                 <input
-                  type="file"
-                  id="file"
-                  name="file"
-                  placeholder="Upload a file"
+                  type='file'
+                  id='file'
+                  name='file'
+                  placeholder='Upload a file'
                   required
                   onChange={this.uploadFile}
                 />
                 {this.state.image && (
-                  <img src={this.state.image} alt="upload preview" />
+                  <img src={this.state.image} alt='upload preview' />
                 )}
               </label>
-              <label htmlFor="title">
+              <label htmlFor='title'>
                 Title
                 <input
-                  type="text"
-                  id="title"
-                  name="title"
-                  placeholder="Title"
+                  type='text'
+                  id='title'
+                  name='title'
+                  placeholder='Title'
                   required
                   value={this.state.title}
                   onChange={this.handleChange}
                 />
               </label>
 
-              <label htmlFor="price">
+              <label htmlFor='price'>
                 Price
                 <input
-                  type="number"
-                  id="price"
-                  name="price"
-                  placeholder="Price"
+                  type='number'
+                  id='price'
+                  name='price'
+                  placeholder='Price'
                   required
                   value={this.state.price}
                   onChange={this.handleChange}
                 />
               </label>
 
-              <label htmlFor="description">
+              <label htmlFor='description'>
                 Description
                 <textarea
-                  id="description"
-                  name="description"
-                  placeholder="Enter a Description"
+                  id='description'
+                  name='description'
+                  placeholder='Enter a Description'
                   required
                   value={this.state.description}
                   onChange={this.handleChange}
                 />
               </label>
               {/* Submit buttons normally send all info to the URL bar */}
-              <button type="submit">Submit</button>
+              <button type='submit'>Submit</button>
             </fieldset>
           </Form>
         )}
