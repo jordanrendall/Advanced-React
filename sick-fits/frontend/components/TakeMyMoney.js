93d24e9e97eda32b6eeee3e65f961e0d0e3268c6
jordanrendall <thomasjrendall@gmail.com>
2019-04-25 16:54:40 -0400

    Fixed brackets

--- sick-fits/frontend/components/TakeMyMoney.js
+++ sick-fits/frontend/components/TakeMyMoney.js
@@ -9,9 +9,34 @@ import calcTotalPrice from '../lib/calcTotalPrice';
 import Error from './ErrorMessage';
 import User, { CURRENT_USER_QUERY } from './User';
 
+function totalItems(cart) {
+  return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
+}
+
 class TakeMyMoney extends React.Component {
   render() {
-    return <User>{({ data: me }) => <p>{this.props.children}</p>}</User>;
+    return (
+      <User>
+        {({ data: me }) =>
+          me.cart ? (
+            <StripeCheckout
+              //ALWAYS NEED TO SEND CENTS TO STRIPE
+              amount={calcTotalPrice(me.cart)}
+              name='Sick Fits'
+              description={`Order of ${totalItems(me.cart)} items!`}
+              image={me.cart[0].item && me.cart[0].item.image}
+              stripeKey='pk_test_jghjTBcGR38ihl8xhGJkGHaR00qYYejQct'
+              currency='CAD'
+              email={me.email}
+            >
+              {this.props.children}
+            </StripeCheckout>
+          ) : (
+            <p>No items</p>
+          )
+        }
+      </User>
+    );
   }
 }
 
