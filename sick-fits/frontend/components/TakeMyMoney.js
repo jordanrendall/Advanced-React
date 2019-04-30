167d9d4ce35bda863decd5c17eed131475df7ec3
jordanrendall <thomasjrendall@gmail.com>
2019-04-30 18:08:14 -0400

    Added forwarding/routing to order page after successful charge

--- sick-fits/frontend/components/TakeMyMoney.js
+++ sick-fits/frontend/components/TakeMyMoney.js
@@ -29,6 +29,8 @@ function totalItems(cart) {
 
 class TakeMyMoney extends React.Component {
   onToken = async (res, createOrder) => {
+    NProgress.start();
+
     console.log('On token called');
     console.log(res.id);
     //manually call mutation once we have stripe token
@@ -39,7 +41,10 @@ class TakeMyMoney extends React.Component {
     }).catch(err => {
       alert(err.message);
     });
-    console.log(order);
+    Router.push({
+      pathname: '/order',
+      query: { id: order.data.createOrder.id },
+    });
   };
   render() {
     return (
