7210f85962b575381c1b5b69c5477fdf84151161
jordanrendall <thomasjrendall@gmail.com>
2019-05-01 06:05:31 -0400

    Created order page

--- sick-fits/frontend/components/Order.js
+++ sick-fits/frontend/components/Order.js
@@ -40,11 +40,48 @@ export default class Order extends Component {
         {({ data, loading, error }) => {
           if (error) return <Error error={error} />;
           if (loading) return <p>Loading...</p>;
-          console.log(data);
+          const order = data.order;
+          console.log(order);
           return (
-            <div>
-              <p>Order ID: {this.props.id}</p>
-            </div>
+            <OrderStyles>
+              <Head>
+                <title>Sick Fits - Order {order.id}</title>
+              </Head>
+              <p>
+                <span>Order ID:</span>
+                <span>{this.props.id}</span>
+              </p>
+              <p>
+                <span>Charge: </span>
+                <span>{order.charge}</span>
+              </p>
+              <p>
+                <span>Created At: </span>
+                <span>{format(order.createdAt, 'MMMM d, YYYY h:mm a')}</span>
+              </p>
+              <p>
+                <span>Order Total: </span>
+                <span>{formatMoney(order.total)}</span>
+              </p>
+              <p>
+                <span>Item Count: </span>
+                <span>{order.items.length}</span>
+              </p>
+              <div className='items'>
+                {order.items.map(item => (
+                  <div className='order-item' key={item.id}>
+                    <img src={item.image} alt={item.title} />
+                    <div className='item-details'>
+                      <h2>{item.title}</h2>
+                      <p>Qty: {item.quantity}</p>
+                      <p>Each: {formatMoney(item.price)}</p>
+                      <p>SubTotal: {formatMoney(item.price * item.quantity)}</p>
+                      <p>{item.description}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </OrderStyles>
           );
         }}
       </Query>
