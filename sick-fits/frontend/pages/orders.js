31fafe2dd4d603c8fb0b6f77286bc9352a9aea00
jordanrendall <thomasjrendall@gmail.com>
2019-05-01 06:05:57 -0400

    Created orders page

---
+++ sick-fits/frontend/pages/orders.js
@@ -0,0 +1,10 @@
+import PleaseSignIn from '../components/PleaseSignin';
+import Orders from '../components/Orders';
+
+const OrdersPage = props => (
+  <PleaseSignIn>
+    <Orders />
+  </PleaseSignIn>
+);
+
+export default OrdersPage;
