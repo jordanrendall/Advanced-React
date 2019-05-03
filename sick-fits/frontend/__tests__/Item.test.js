964fa3ec07b5224016a37b5a99fcf1355936f992
jordanrendall <thomasjrendall@gmail.com>
2019-05-02 20:17:39 -0400

    Introduced snapshot testing

--- sick-fits/frontend/__tests__/Item.test.js
+++ sick-fits/frontend/__tests__/Item.test.js
@@ -1,39 +1,44 @@
 import ItemComponent from '../components/Item';
 import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 
 const fakeItem = {
   id: 'ABC123',
   title: 'A cool item',
-  price: 5000,
+  price: 4000,
   description: 'this item is really cool',
   image: 'dog.jpg',
   largeImage: 'largedog.jpg',
 };
 
 describe('<Item/>', () => {
-  it('renders and displays properly', () => {
+  it('renders and matches the snapshot', () => {
     const wrapper = shallow(<ItemComponent item={fakeItem} />);
-    const img = wrapper.find('img');
-    console.log(img.props());
-    expect(img.props().src).toBe(fakeItem.image);
-    expect(img.props().src).toBe(fakeItem.image);
+    expect(toJSON(wrapper)).toMatchSnapshot();
   });
+  // it('renders and displays properly', () => {
+  //   const wrapper = shallow(<ItemComponent item={fakeItem} />);
+  //   const img = wrapper.find('img');
+  //   console.log(img.props());
+  //   expect(img.props().src).toBe(fakeItem.image);
+  //   expect(img.props().src).toBe(fakeItem.image);
+  // });
 
-  it('renders the price tag and title properly', () => {
-    const wrapper = shallow(<ItemComponent item={fakeItem} />);
-    const PriceTag = wrapper.find('PriceTag');
-    // console.log(PriceTag.debug());
-    console.log(PriceTag.children().text());
-    expect(PriceTag.children().text()).toBe('$50');
-    expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
-  });
-  it('renders out the buttons properly', () => {
-    const wrapper = shallow(<ItemComponent item={fakeItem} />);
-    const buttonList = wrapper.find('.buttonList');
-    expect(buttonList.children()).toHaveLength(3);
-    expect(buttonList.find('Link')).toHaveLength(1);
-    expect(buttonList.find('Link')).toBeTruthy();
-    expect(buttonList.find('Link').exists()).toBe(true);
-    console.log(buttonList.children());
-  });
+  // it('renders the price tag and title properly', () => {
+  //   const wrapper = shallow(<ItemComponent item={fakeItem} />);
+  //   const PriceTag = wrapper.find('PriceTag');
+  //   // console.log(PriceTag.debug());
+  //   console.log(PriceTag.children().text());
+  //   expect(PriceTag.children().text()).toBe('$50');
+  //   expect(wrapper.find('Title a').text()).toBe(fakeItem.title);
+  // });
+  // it('renders out the buttons properly', () => {
+  //   const wrapper = shallow(<ItemComponent item={fakeItem} />);
+  //   const buttonList = wrapper.find('.buttonList');
+  //   expect(buttonList.children()).toHaveLength(3);
+  //   expect(buttonList.find('Link')).toHaveLength(1);
+  //   expect(buttonList.find('Link')).toBeTruthy();
+  //   expect(buttonList.find('Link').exists()).toBe(true);
+  //   console.log(buttonList.children());
+  // });
 });
