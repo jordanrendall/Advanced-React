7c5eee29c9437da4ea20776d040ed3b0d779594e
jordanrendall <thomasjrendall@gmail.com>
2019-05-05 07:38:03 -0400

    Added data-test property

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -12,7 +12,7 @@ export default function Nav() {
     //put user render component as high as possible to allow toggling of display of links
     <User>
       {({ data: { me } }) => (
-        <NavStyles>
+        <NavStyles data-test='nav'>
           <Link href='/items'>
             <a>Shop</a>
           </Link>
