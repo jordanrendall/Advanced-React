a687a9dc867cdb2017c63d8da5f65baaf213e376
jordanrendall <thomasjrendall@gmail.com>
2019-05-06 07:05:08 -0400

    AddToCart tests

--- sick-fits/frontend/components/AddToCart.js
+++ sick-fits/frontend/components/AddToCart.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Mutation } from "react-apollo";
-import gql from "graphql-tag";
-import { CURRENT_USER_QUERY } from "./User";
+import React from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
   mutation addToCart($id: ID!) {
@@ -23,7 +23,7 @@ class AddToCart extends React.Component {
       >
         {(addToCart, { loading }) => (
           <button disabled={loading} onClick={addToCart}>
-            Add{loading && "ing"} To Cart
+            Add{loading && 'ing'} To Cart
           </button>
         )}
       </Mutation>
@@ -32,3 +32,4 @@ class AddToCart extends React.Component {
 }
 
 export default AddToCart;
+export { ADD_TO_CART_MUTATION };
